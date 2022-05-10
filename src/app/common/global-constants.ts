39b986c7149c1374f21a4f9fcea03b97211916b2
vasudhareddy <karri.vasudha@gmail.com>
2022-05-10 22:44:34 +0530

    role and manger name change

--- src/app/common/global-constants.ts
+++ src/app/common/global-constants.ts
@@ -1,10 +1,9 @@
 export class GlobalConstants {
 public static users:any =[
-  {"userId":"U362957","password":"U362957","roles":["USER"]},
   {"userId":"U832221","password":"U832221","roles":["ADMIN"]},
-  {"userId":"U832225","password":"U832225","roles":["ADMIN"]},
-  {"userId":"U832224","password":"U832224","roles":["ADMIN"]},
-  {"userId":"U831231","password":"U831231","roles":["ADMIN"]},
-  {"userId":"U832222","password":"U832222","roles":["ADMIN"]}
+  {"userId":"U832225","password":"U832225","roles":["USER"]},
+  {"userId":"U832224","password":"U832224","roles":["USER"]},
+  {"userId":"U831231","password":"U831231","roles":["USER"]},
+  {"userId":"U832222","password":"U832222","roles":["USER"]}
   ];
 }
